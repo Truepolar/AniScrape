e04ea8a40c1259c1f04f83529b39161404d9a32c
Truepolar <ryansoh2.mail@gmail.com>
2025-02-03 16:30:31 +0800

    test build for list 1

--- QuestionClass.py
+++ QuestionClass.py
@@ -1,7 +1,9 @@
 import numpy as np
 
 u1 = []
+w1 = []
 
+#Question Class
 
 class Question():
     def __init__(self, question, options, check, answer):
@@ -10,7 +12,7 @@ class Question():
         self.check = check
         self.answer = answer
 
-    def checker(self, ans):
+    def qchecker(self, ans):
         if ans not in self.check:
             return False
         return True
@@ -19,17 +21,25 @@ class Question():
         print(self.question)
         print(self.options)
 
+    def weight_question(self):
+        print(self.question)
+
 
 q1 = Question("1.Political alignment", " a.Left \n b.Right", {"a", "b"}, 0)
 q2 = Question("2.Gender", " a.Male\n b.Female", {"a", "b"}, 0)
+q3 = Question("3.Monthly income","a.0-3k \n b.3k-5k \n c.5k-8k \n d.8k-12k \n e.12k and above \n f.Does not matter",{"a","b","c","d","e","f"},0)
+q4 = Question("4.Height in cm","a.140-150 \n b.150-160 \n c.160-170 \n d.170-180 \n e.180 and above \n f.Does not matter",{"a","b","c","d","e","f"},0)
+q5 = Question("5.interests","a.Reading \n b.Music \n c.Making music \n d.Gaming \n e.Sports \n f.Food \n g.others",{"a","b","c","d","e","f","g"},0)
 
-for q in [q1, q2]:
+all_questions = [q1,q2,q3,q4,q5]
+
+for q in [q1, q2,q3,q4,q5]:
     prompt = f"""
     {q.askquestion()}
     """
     while True:
         val = input()
-        gate = q.checker(val)
+        gate = q.qchecker(val)
         if gate:
             print("very interesting")
             break
@@ -41,3 +51,14 @@ for q in [q1, q2]:
     print('_' * 40)
 
 print(u1)
+
+# Weight for question class (new naming convention)
+
+print('_' * 40)
+
+print("Please rank the questions in order of least important to most important")
+
+for q in all_questions:
+    prompt = f"""
+    {q.weight_question()}
+"""
