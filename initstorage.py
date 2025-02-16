eaf0ba92fcf70cb5bd8bfa2804b9420713da6693
Truepolar <ryansoh2.mail@gmail.com>
2025-02-16 18:56:04 +0800

    HOWWWWWW

--- initstorage.py
+++ initstorage.py
@@ -2,21 +2,21 @@
 
 
 from sqlalchemy import create_engine , Column , Integer, String,ForeignKey
-from sqlalchemy.orm import declarative_base,relationship,sessionmaker,mapped_column,Mapped,relationships
+from sqlalchemy.orm import declarative_base,relationship,sessionmaker,mapped_column,Mapped,DeclarativeBase
 from decouple import config
 
-url=config("url")
+url = config("url")
 
-engine = create_engine(url)
+engine = create_engine(url, echo=True)
 
-Base = declarative_base()
+class Base(DeclarativeBase):
+    pass
 
 class UserS(Base):
     __tablename__ = "userdata"
 
     uid: Mapped[int] = mapped_column(primary_key=True)
-    answer_uid: Mapped[int] = mapped_column(ForeignKey("answerdata.user_id"))
-    name: Mapped[str] = mapped_column(primary_key=True)
+    name: Mapped[str]
     password: Mapped[str]
     age: Mapped[int]
     gender: Mapped[str]
@@ -26,7 +26,6 @@ class QuestionS(Base):
     __tablename__ = "questiondata"
 
     qid: Mapped[int] = mapped_column(primary_key=True)
-    question_id: Mapped[int] = mapped_column(ForeignKey("answerdata.question_id"))
     question: Mapped[str]
     options: Mapped[str]
     checks: Mapped[str]
@@ -34,15 +33,13 @@ class QuestionS(Base):
 
 class AnswerS(Base):
     __tablename__ = "answerdata"
-
-    user_id: Mapped[int] = mapped_column(primary_key=True)
-    question_id: Mapped[int] = mapped_column(primary_key=True)
+    aid: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey('userdata.uid'))
+    question_id: Mapped[int] = mapped_column(ForeignKey('questiondata.qid'))
     answer: Mapped[str]
     weight: Mapped[int]
     answerl1: Mapped[list["UserS"]] = relationship(back_populates="userl")
     answerl2: Mapped[list["QuestionS"]] = relationship(back_populates="questionl")
-
-
 
 Base.metadata.create_all(engine)
 
